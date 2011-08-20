cb8f8c2269b2cdcf366ff35d37986aabe4698a00
Daniel Morris <daniel.john.morris@googlemail.com>
2011-08-20 23:57:24 +0100

    Use your own api keys! :)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,8 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :facebook, '172218082831373', '98a2c55859b543633d4549b85be1c0cb'
-  provider :facebook, '172218082831373', '98a2c55859b543633d4549b85be1c0cb'
-  provider :linked_in, 'ht3xuvg87571', 'GK9fhGxhi9qnET42'
+  provider :twitter, APP_CONFIG['twitter_key'], APP_CONFIG['twitter_secret']
+  provider :facebook, APP_CONFIG['facebook_key'], APP_CONFIG['facebook_secret']
+  provider :linked_in, APP_CONFIG['linked_in_key'], APP_CONFIG['linked_in_secret']
   provider :google_apps, OpenID::Store::Filesystem.new('/tmp')
   provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
